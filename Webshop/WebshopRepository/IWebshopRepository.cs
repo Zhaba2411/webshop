e0b52d08dd42107d2edd86a41c8ac5a02816d702
Janko Žabojec <jz49497@fer.hr>
2018-02-15 11:54:54 +0100

    start

---
+++ Webshop/WebshopRepository/IWebshopRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopRepository
+{
+    public interface IWebshopRepository
+    {
+        WebshopItem Get(Guid todoId, Guid userId);
+
+        void Add(WebshopItem webshopItem);
+
+    }
+}
---
+++ Webshop/WebshopRepository/WebshopItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopRepository
+{
+    public class WebshopItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool Available { get; set; }
+        public int NumberOfItems { get; set; }
+        public bool Sale { get; set; }
+        public double SalePrice { get; set; }
+        public string Description { get; set; }
+
+        public WebshopItem() { }
+        
+
+    }
+}
